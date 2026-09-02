a70db7981026c64f371339c450f1a49d27faa418
Jason Zhang <zjjc123@hotmail.com>
2026-09-02 15:57:38 -0400

    rms norm

--- src/tiny_llm/layer_norm.py
+++ src/tiny_llm/layer_norm.py
@@ -3,7 +3,17 @@ import mlx.core as mx
 
 class RMSNorm:
     def __init__(self, dim: int, weight: mx.array, eps: float = 1e-5):
-        pass
+        self.dim = dim
+        self.weight = weight
+        self.eps = eps
 
     def __call__(self, x: mx.array) -> mx.array:
-        pass
+        '''
+        Since it's a type of layer norm we want to normalize across D not N (batch)
+        '''
+        # N..., D
+        x_f32 = x.astype(mx.float32)
+
+        # Broadcast weight from D to N..., D
+        rms = mx.rsqrt(mx.mean(mx.square(x_f32), axis = -1) + self.eps)[..., None]
+        return (x * rms * self.weight.T).astype(x.dtype)
