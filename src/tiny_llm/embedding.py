540eb178de1855b068e674230a9504e3fab73ac4
Jason Zhang <zjjc123@hotmail.com>
2026-09-03 12:16:27 -0400

    qwen3 embedding

--- src/tiny_llm/embedding.py
+++ src/tiny_llm/embedding.py
@@ -4,13 +4,32 @@ from .quantize import QuantizedWeights
 
 class Embedding:
     def __init__(self, vocab_size: int, embedding_dim: int, weight: mx.array):
-        pass
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.weight = weight
 
     def __call__(self, x: mx.array) -> mx.array:
-        pass
+        '''
+        weight: vocab_size * embedding_dim
+        input: N...
+        output: N... * embedding_dim
+
+        index look up on weight from token id x
+        equivalent to multiplying one-hot by weight
+        '''
+        return self.weight[x]
+
+
 
     def as_linear(self, x: mx.array) -> mx.array:
-        pass
+        '''
+        weight: vocab_size * embedding_dim
+        input: N.. * embedding_dim
+        output: N.. * vocab_size
+        '''
+        weight_t = mx.swapaxes(self.weight, -1, -2)
+        return x @ weight_t
+
 
 
 class QuantizedEmbedding:
